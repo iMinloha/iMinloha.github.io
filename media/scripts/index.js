80389f8b37ab7105a01d91b75ce8abbd4d577884
iMinloha <2821163570@qq.com>
2021-01-28 11:51:28 +0800

    update from gridea: 2021-01-28 11:51:28

--- media/scripts/index.js
+++ media/scripts/index.js
@@ -1,1 +1,7 @@
-console.log('Hello Gridea')
+console.log('Hi welcome to minloha.cn');
+$(document).ready(function(){
+    $(".list_right").hide();
+    $("#right_list").click(function(){
+        $(".list_right").fadeToggle("slow");
+    });
+});
