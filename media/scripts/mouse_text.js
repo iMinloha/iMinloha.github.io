bece23cd8e3d926f3500ee3c045e9207df88087b
iMinloha <67627181+iMinloha@users.noreply.github.com>
2021-04-15 11:42:26 +0800

    Add files via upload

--- media/scripts/mouse_text.js
+++ media/scripts/mouse_text.js
@@ -6,13 +6,13 @@ function randomColor(){
     var b = Math.floor(Math.random() * 256);
     var color = '#' + r.toString(16) + g.toString(16) + b.toString(16);
     return color;
-    // return "rgb(72,85,137)"
+    //return "rgb(72,85,137)"
 }
 jQuery(document).ready(function($) {
 
 
     $("body").click(function(e) {
-        var a = new Array("富强","民主","和谐","文明","自由","平等","公正","法治","爱国","敬业","诚信","友善");
+        var a = new Array("C","编曲","原神","Html","php","富强","C++","民主","和谐","python","文明","自由","C#","平等","java","公正","法治","javascript","爱国","敬业","study","诚信","友善");
         var $i = $("<span></span>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
