898235ab682c15096bf0eefed7d95d4d96ac9a19
iMinloha <67627181+iMinloha@users.noreply.github.com>
2021-05-02 20:15:09 +0800

    Add files via upload

--- media/scripts/index.js
+++ media/scripts/index.js
@@ -2,6 +2,9 @@ console.log('Hi welcome to minloha.cn');
 $(document).ready(function(){
     $(".list_right").hide();
     $("#right_list").click(function(){
-        $(".list_right").fadeToggle("slow");
+        $(".list_right").fadeToggle(500);
+    });
+    $("#right_list").blur(function(){
+        $(".list_right").fadeOut(500);
     });
 });
